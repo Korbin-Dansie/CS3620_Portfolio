de68b42c58c7648175124d2a472d338df484c842
Korbin Dansie <dansiekm@gmail.com>
2023-10-09 17:09:51 -0600

    Change where images are stored

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Portfolio(models.Model) : 
     name  = models.CharField(max_length=255) # have to use max length for a CharField
     description = models.TextField(blank=False, null=False)
-    image = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to="portfolio", null=True, blank=True)
     image_alt = models.CharField(max_length=255, blank=True) # Used for alt text of the image
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Portfolio(models.Model) :
 class Hobby(models.Model) : 
     name  = models.CharField(max_length=255) # have to use max length for a CharField
     description = models.TextField(blank=False, null=False)
-    image = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to="hobby", max_length=255, null=True, blank=True)
     image_alt = models.CharField(max_length=255, blank=True) # Used for alt text of the image
     def __str__(self):
         return self.name
